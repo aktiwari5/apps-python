a1de040317b22bd523c5b936212590abab1823db
aktiwari5 <ajit.kumar.tiwari@outlook.com>
2020-05-26 12:01:56 -0400

    Commit2

--- hello-python.py
+++ hello-python.py
@@ -1,3 +1,5 @@
 print("Welcome, let's learn this cool language of Python!")
 print("Also, learn how to use git and github for version control and collaborative development")
 print("And, how does Nodejs and Express is used to setup clien-server runtime for webapp development")
+
+
