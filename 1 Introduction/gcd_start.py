18b0fef858b22596e02108149090c80171023ed0
aktiwari5 <ajit.kumar.tiwari@outlook.com>
2020-05-27 02:10:29 -0400

    updated on 27 May 2020

--- 1 Introduction/gcd_start.py
+++ 1 Introduction/gcd_start.py
@@ -3,8 +3,16 @@
 
 
 def gcd(a, b):
-    pass
+
+    if a < a:
+        (a, b) = (b, a)
     
+    while (a % b) != 0:
+        (a, b) = (b, (a%b))
+
+    return(b)
+
+        
         
 # try out the function with a few examples
 print(gcd(60, 96))  # should be 12
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # apps-python
 
-Goal is to Learn cool Python language and implementaion in various arear.
+Goal is to Learn cool Python language and implementaion in various area.
